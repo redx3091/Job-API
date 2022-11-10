cebfb17a39e810e43337f204905ff58caf356656
Christian A <christiana@Mac-Pro-de-Christian.local>
2022-11-09 23:58:24 -0500

    ADD Logic for login method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,37 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-const  { BadRequestError } = require('../errors/index.error');
-const JWT = require('jsonwebtoken');
-
+const {
+  BadRequestError,
+  UnauthenticatedError,
+} = require('../errors/index.error');
 
 const register = async (req, res) => {
-  const user = await User.create({...req.body});
+  const user = await User.create({ ...req.body });
   const token = user.createJWT();
-  res.status(StatusCodes.CREATED).json({user:{ name: user.name }, token});
-}
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+};
 const login = async (req, res) => {
-  res.send('login user');
-}
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Provide email and password');
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+
+  //* Compare password
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credential');
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+};
 
 module.exports = {
   register,
   login,
-}
-
+};
