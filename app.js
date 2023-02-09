02bdc67058264be6587709bfee89581576abff26
Christian <christian@Mac-Pro-de-Christian.local>
2023-02-09 13:30:31 -0500

    ADD security package

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+//* Extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -17,6 +24,16 @@ const ErrorHandler = require('./middleware/error-handler');
 
 app.use(express.json());
 //*extra packages
+app.set('trus proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //* 15 minutes
+    max: 100, //* limit each IP to 100 request per windoiwMs
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 //*routes
 app.use('/api/v1/auth', authRouter);
